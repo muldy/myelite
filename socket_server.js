65299e574796067607416b46d8efc32da948b6f1
muldy <muldy@rogue01>
2018-02-20 11:06:41 +0000

    line by line log reader

--- socket_server.js
+++ socket_server.js
@@ -12,3 +12,29 @@ test.on('connection', function(client){
   });
 });
 io.listen(3000);
+
+
+
+var LineByLineReader = require('line-by-line'),
+      lr = new LineByLineReader('event_logs/event1.log');
+
+  lr.on('error', function (err) {
+  	// 'err' contains error object
+  });
+
+  lr.on('line', function (line) {
+  	// pause emitting of lines...
+  	lr.pause();
+
+  	// ...do your asynchronous line processing..
+    console.log(line);
+  	setTimeout(function () {
+
+  		// ...and continue emitting lines.
+  		lr.resume();
+  	}, 1000);
+  });
+
+  lr.on('end', function () {
+  	// All lines are read, file is closed now.
+  });
