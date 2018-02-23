b6a27b99843441fcf8f1a5e2ae7680fc4abc7062
muldy <muldy@rogue01>
2018-02-23 12:55:53 +0000

    working!

--- main.js
+++ main.js
@@ -80,6 +80,26 @@ var Datastore = require('nedb'),
   });
 
 
+  //load handlebars partials
+
+var hbs = require('hbs');
+var fs = require('fs');
+
+var partialsDir = __dirname + '/views/partials';
+
+var filenames = fs.readdirSync(partialsDir);
+
+filenames.forEach(function (filename) {
+  var matches = /^([^.]+).hbs$/.exec(filename);
+  if (!matches) {
+    return;
+  }
+  var name = matches[1];
+  var template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
+  hbs.registerPartial(name, template);
+});
+
+
   
 
 var wapp = require('express')();
