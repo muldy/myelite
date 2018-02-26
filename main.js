90039279b2bb81fc9d3340ba107aded00dea9626
muldy <muldy@rogue01>
2018-02-26 11:52:08 +0000

    major cleanup

--- main.js
+++ main.js
@@ -87,32 +87,6 @@ dbCommunityGoal = new Datastore({
 });
 
 
-//load handlebars partials
-
-var hbs = require('hbs');
-var fs = require('fs');
-
-var partialsDir = __dirname + '/views/partials';
-
-var filenames = fs.readdirSync(partialsDir);
-
-filenames.forEach(function (filename) {
-  var matches = /^([^.]+).hbs$/.exec(filename);
-  if (!matches) {
-    return;
-  }
-  var name = matches[1];
-  var template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
-  hbs.registerPartial(name, template);
-});
-
-var exphbs = require('express-handlebars');
-
-var hbs = exphbs.create({
-  defaultLayout: 'main'
-});
-
-
 const {
   ipcMain
 } = require('electron')
