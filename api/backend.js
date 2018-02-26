8ccda1c028069494a4a5a982e46f845a0da56032
muldy <muldy@rogue01>
2018-02-26 17:57:42 +0000

    added community_goals callbak

--- api/backend.js
+++ api/backend.js
@@ -6,21 +6,34 @@ const {
 exports.bindBackend = function () {
     var process = require('./data_process')
     ipcMain.on('get_data', (event, arg) => {
-        console.log(arg) // prints "ping"
-        dbMissions.find({}).sort({
-            DestinationSystem: 1,
-            DestinationStation: 1
-        }).exec(function (err, docs) {
+        console.log("Get data: ", arg)
+        if (arg == "active_missions") {
+            dbMissions.find({}).sort({
+                DestinationSystem: 1,
+                DestinationStation: 1
+            }).exec(function (err, docs) {
 
-            var gistMissions = process.getMissionGist(docs)
-            //gistMissions=Object.keys(gistMissions).map(x=>gistMissions[x])
-           
-            event.sender.send('data', {
-                type: "active_missions",
-                missions: docs,
-                missionCount: docs.length,
-                missionTargetSystems:gistMissions
+                var gistMissions = process.getMissionGist(docs)
+                //gistMissions=Object.keys(gistMissions).map(x=>gistMissions[x])
+
+                event.sender.send('data', {
+                    type: "active_missions",
+                    missions: docs,
+                    missionCount: docs.length,
+                    missionTargetSystems: gistMissions
+                })
             })
-        })
+        }
+        else if (arg == "community_goals") {
+            dbCommunityGoal.find({}).sort({
+                Expiry
+            }).exec(function (err, docs) {
+
+                event.sender.send('data', {
+                    type: "community_goals",
+                    goals: docs,
+                })
+            })
+        }
     })
 }
